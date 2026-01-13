74407264253f6fdc228a8ebfdf12782c1fc1a318
Diego Serpelloni <121094870+serpico3@users.noreply.github.com>
2026-01-13 10:07:23 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR, TESTTTTTT
+          Edit <code>src/App.jsx</code> and save to test HMR, TESTTTTTT, testino
         </p>
       </div>
       <p className="read-the-docs">
